8d64ab3bda8281828ef5f31d9fbe26ecd9dcd566
Poomagal-Nanmaran <106588778+Poomagal-Nanmaran@users.noreply.github.com>
2024-11-13 23:31:03 +0530

    Update run_ai.sh

--- AImonk/AI/run_ai.sh
+++ AImonk/AI/run_ai.sh
@@ -1,4 +1,4 @@
 container="ai"
 docker stop $container
 docker rm $container
-docker run -it --name $container -v /home/poo/AImonk/files/:/home/files/ -p 8000:8000 aimonk/ai
+docker run -it --name $container -v $(pwd)/AImonk/files/:/home/files/ -p 8000:8000 aimonk/ai
